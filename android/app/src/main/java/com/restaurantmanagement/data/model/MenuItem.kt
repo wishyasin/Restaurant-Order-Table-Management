f410684c1707934a8e7fe3c95fe7d04598227b87
Burak Berkay Ak <burakberkayak@gmail.com>
2026-03-26 00:01:59 +0300

    feature: necessary changes

--- android/app/src/main/java/com/restaurantmanagement/data/model/MenuItem.kt
+++ android/app/src/main/java/com/restaurantmanagement/data/model/MenuItem.kt
@@ -4,6 +4,14 @@ data class MenuItem(
     val id: Int,
     val name: String,
     val price: Double,
-    val category: String,
+    val category: MenuCategory,
+    val description: String = "",
     val isAvailable: Boolean = true
 )
+
+enum class MenuCategory {
+    FOOD,
+    DRINK,
+    DESSERT,
+    SNACK
+}
